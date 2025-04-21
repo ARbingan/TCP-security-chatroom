e9937e685f5eb28ef085ee6025d6eb7250ba787d
灯火不休_LY <14928300+endless-lights---ly@user.noreply.gitee.com>
2025-04-21 20:09:29 +0800

    增加注释，删除冗余代码

--- src/main/java/org/example/chart/client/client2.java
+++ src/main/java/org/example/chart/client/client2.java
@@ -3,6 +3,8 @@ package org.example.chart.client;
 
 import org.example.chart.JFrame.SocketJFrame;
 
+
+
 public class client2 {
     public static void main(String[] args) {
         new SocketJFrame();
